dc014b1cb36326f9b14d757bbc801deb7df424eb
mmgalindocastillo7 <mgalindocastillo6@gmail.com>
2020-06-15 18:37:20 -0500

    Pueba Ceiba
    Laboratorio de seleccion para CEIBA

---
+++ CeibaProject-master/provider-ms/src/main/java/com/ceiba/software/lab/providers/ProviderMsApplication.java
@@ -0,0 +1,13 @@
+package com.ceiba.software.lab.providers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProviderMsApplication.class, args);
+	}
+
+}
---
+++ CeibaProject-master/provider-ms/src/test/java/com/ceiba/software/lab/providers/ProviderMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceiba.software.lab.providers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
